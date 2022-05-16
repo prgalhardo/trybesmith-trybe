168baed5b22ec915c59e81dbbc9d8bae5b49bb72
Priscila Silva <priscilacds_100@yahoo.com.br>
2022-05-16 17:54:43 -0300

    feat(connection): criaçao do arquivo connection.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import app from './app';
-import 'dotenv/config';
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 const server = app.listen(PORT, () => console.log(
   `Server is running on PORT: ${PORT}`,
