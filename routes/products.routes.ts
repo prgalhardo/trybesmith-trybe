4db9e3358a1adeda7a2df19bd90416958b60fbaa
Priscila Silva <priscilacds_100@yahoo.com.br>
2022-05-17 00:53:26 -0300

    feat(productsRoutes): implementaçao do arquivo products.routes

--- routes/products.routes.ts
+++ routes/products.routes.ts
@@ -1,5 +1,9 @@
 import { Router } from 'express';
+import ProductsController from '../src/controllers/products.controllers';
 
 const productsRoutes = Router();
+const productsController = new ProductsController();
+
+productsRoutes.get('/products', productsController.getAll);
 
 export default productsRoutes;
