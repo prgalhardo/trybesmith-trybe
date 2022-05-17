accecca4cafc92790ab7f69feea5af151818c9ce
Priscila Silva <priscilacds_100@yahoo.com.br>
2022-05-16 23:16:56 -0300

    feat(userController): correçao arquivo users.controllers

--- src/controllers/users.controllers.ts
+++ src/controllers/users.controllers.ts
@@ -1,13 +1,14 @@
 import { Request, Response } from 'express';
 import UserService from '../services/users.service';
+import generateToken from '../services/generateToken';
 
 export default class UsersController {
   constructor(private userService = new UserService()) { }
   
   public create = async (req: Request, res: Response) => {
     const user = req.body;
-
     const userCreated = await this.userService.create(user);
-    res.status(201).json(userCreated);
+    const token = generateToken({ id: userCreated.id, username: userCreated.username });
+    res.status(201).json(token);
   };
 }
