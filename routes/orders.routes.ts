5fbcef24fe851cc00dc5f06b58de3886589793fe
Priscila Silva <priscilacds_100@yahoo.com.br>
2022-05-21 22:17:56 -0300

    feat(requisito4): implementaçao das rotas de orders e atualizaçao em app.ts

--- routes/orders.routes.ts
+++ routes/orders.routes.ts
@@ -1,5 +1,9 @@
 import { Router } from 'express';
+import OrdersController from '../src/controllers/orders.controllers';
 
 const ordersRoutes = Router();
+const ordersController = new OrdersController();
 
-ordersRoutes.get('/orders');
+ordersRoutes.get('/orders', ordersController.getAll);
+
+export default ordersRoutes;
--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import usersRoutes from '../routes/users.routes';
 import productsRoutes from '../routes/products.routes';
+import ordersRoutes from '../routes/orders.routes';
 
 const app = express();
 
 app.use(express.json());
 app.use(usersRoutes);
 app.use(productsRoutes);
+app.use(ordersRoutes);
 
 export default app;
