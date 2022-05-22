b9f242975dd15a9d14cd77bd047f236e55549880
Priscila Silva <priscilacds_100@yahoo.com.br>
2022-05-21 22:34:15 -0300

    feat(requisito5): implementaçao de login.routes e atualizaçao no app.ts

---
+++ routes/login.routes.ts
@@ -0,0 +1,7 @@
+import { Router } from 'express';
+
+const loginRoutes = Router();
+
+loginRoutes.post('login');
+
+export default loginRoutes;
--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import usersRoutes from '../routes/users.routes';
 import productsRoutes from '../routes/products.routes';
 import ordersRoutes from '../routes/orders.routes';
+import loginRoutes from '../routes/login.routes';
 
 const app = express();
 
@@ -9,5 +10,6 @@ app.use(express.json());
 app.use(usersRoutes);
 app.use(productsRoutes);
 app.use(ordersRoutes);
+app.use(loginRoutes);
 
 export default app;
